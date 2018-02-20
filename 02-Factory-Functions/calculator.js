3e099a405ccd8fb459b05bb3b41e887c17098adc
Samuel Yun <shysam.yun@gmail.com>
2018-02-20 17:22:42 -0500

    completed 02

---
+++ 02-Factory-Functions/calculator.js
@@ -0,0 +1,9 @@
+function createCalculator() {
+  return {
+    total: 0,
+    value: function() {return this.total},
+    add: function(num) {this.total += num},
+    subtract: function(num) {this.total -= num},
+    clear: function() {this.total = 0}
+  }
+}
